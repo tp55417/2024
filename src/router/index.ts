377b5208af3e9a631905e96de69c7094553221d3
Mirumo <mysteryperson520@gmail.com>
2024-07-03 14:09:20 +0800

    Feat: config page if showing with .env

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,173 +2,133 @@ import { setupI18nRoutes } from '@/modules/i18n'
 import { RouterOptions } from 'vite-ssg'
 import { RouteRecordRaw, RouterScrollBehavior } from 'vue-router'
 
-const routes: RouteRecordRaw[] = import.meta.env.VITE_LANDING_ONLY === 'true'
-  ? [
-      {
-        path: '/',
-        name: 'Home',
-        redirect: {
-          name: 'Landing'
-        },
-        meta: {
-          order: 0
-        }
-      },
-      {
-        path: '/landing',
-        name: 'Landing',
-        component: () => import('@/pages/Landing.vue')
-      },
-      {
-        path: '/map',
-        name: 'Map',
-        component: () => import('@/pages/Map.vue'),
-        meta: {
-          order: 3
-        }
-      },
-      {
-        path: '/sponsorship',
-        name: 'Sponsorship',
-        component: () => import('@/pages/Sponsorship.vue'),
-        meta: {
-          order: 1
-        }
-      },
-      {
-        path: '/sponsor',
-        name: 'Sponsor',
-        component: () => import('@/pages/Sponsor.vue'),
-        meta: {
-          order: 5
-        }
-      },
-      {
-        path: '/staff',
-        name: 'Staff',
-        component: () => import('@/pages/Staff.vue'),
-        meta: {
-          order: 8
-        }
-      }
-    ]
-  : [
-      // {
-      //   path: '/',
-      //   name: 'Home',
-      //   redirect: {
-      //     name: 'Landing'
-      //   },
-      //   meta: {
-      //     order: 0
-      //   }
-      // },
-      {
-        path: '/',
-        name: 'Home',
-        component: () => import('@/pages/Home.vue'),
-        meta: {
-          order: 0
-        }
-      },
-      {
-        path: '/landing',
-        name: 'Landing',
-        component: () => import('@/pages/Landing.vue')
-      },
-      {
-        path: '/guide',
-        name: 'Guide',
-        component: () => import('@/pages/Guide.vue'),
-        meta: {
-          order: 0
-        }
-      },
-      {
-        path: '/session',
-        name: 'Session',
-        component: () => import('@/pages/Session.vue'),
-        children: [
-          {
-            path: ':sessionId',
-            name: 'SessionDetail',
-            component: () => import('@/pages/Session.vue')
+const env = import.meta.env
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    ...(env.VITE_IS_LANDING === 'true'
+      ? {
+          redirect: {
+            name: 'Landing'
           }
-        ],
-        meta: {
-          order: 1
         }
-      },
+      : {
+          component: () => import('@/pages/Home.vue')
+        }),
+    meta: {
+      order: 0
+    }
+  },
+  {
+    path: '/landing',
+    name: 'Landing',
+    component: () => import('@/pages/Landing.vue')
+  },
+  {
+    path: '/guide',
+    name: 'Guide',
+    component: () => import('@/pages/Guide.vue'),
+    meta: {
+      order: 0,
+      hidden: env.VITE_SHOW_GUIDE !== 'true'
+    }
+  },
+  {
+    path: '/session',
+    name: 'Session',
+    component: () => import('@/pages/Session.vue'),
+    children: [
       {
-        path: '/room',
-        name: 'Room',
-        component: () => import('@/pages/Room.vue'),
-        meta: {
-          order: 2
-        }
-      },
-      {
-        path: '/topics',
-        name: 'Topics',
-        component: () => import('@/pages/Topics.vue'),
-        meta: {
-          order: 3
-        }
-      },
-      {
-        path: '/sponsorship',
-        name: 'Sponsorship',
-        component: () => import('@/pages/Sponsorship.vue'),
-        meta: {
-          hidden: true
-        }
-      },
-      {
-        path: '/venue',
-        name: 'Venue',
-        component: () => import('@/pages/Venue.vue'),
-        meta: {
-          order: 4
-        }
-      },
-      {
-        path: '/map',
-        name: 'Map',
-        component: () => import('@/pages/Map.vue'),
-        meta: {
-          order: 5
-        }
-      },
-      {
-        path: '/sponsor',
-        name: 'Sponsor',
-        component: () => import('@/pages/Sponsor.vue'),
-        meta: {
-          order: 6
-        }
-      },
-      {
-        path: '/community',
-        name: 'Community',
-        component: () => import('@/pages/Community.vue'),
-        meta: {
-          order: 7
-        }
-      },
-      {
-        path: '/staff',
-        name: 'Staff',
-        component: () => import('@/pages/Staff.vue'),
-        meta: {
-          order: 8
-        }
-      },
-      {
-        name: 'NotFound',
-        path: '/:catchAll(.*)',
-        redirect: '/'
+        path: ':sessionId',
+        name: 'SessionDetail',
+        component: () => import('@/pages/Session.vue')
       }
-    ]
+    ],
+    meta: {
+      order: 1,
+      hidden: env.VITE_SHOW_SESSION !== 'true'
+    }
+  },
+  {
+    path: '/sponsorship',
+    name: 'Sponsorship',
+    component: () => import('@/pages/Sponsorship.vue'),
+    meta: {
+      order: 1,
+      hidden: env.VITE_SHOW_SPONSORSHIP !== 'true'
+    }
+  },
+  {
+    path: '/room',
+    name: 'Room',
+    component: () => import('@/pages/Room.vue'),
+    meta: {
+      order: 2,
+      hidden: env.VITE_SHOW_ROOM !== 'true'
+    }
+  },
+  {
+    path: '/topics',
+    name: 'Topics',
+    component: () => import('@/pages/Topics.vue'),
+    meta: {
+      order: 3,
+      hidden: env.VITE_SHOW_TOPIC !== 'true'
+    }
+  },
+  {
+    path: '/map',
+    name: 'Map',
+    component: () => import('@/pages/Map.vue'),
+    meta: {
+      order: 3,
+      hidden: env.VITE_SHOW_MAP !== 'true'
+    }
+  },
+  {
+    path: '/venue',
+    name: 'Venue',
+    component: () => import('@/pages/Venue.vue'),
+    meta: {
+      order: 4,
+      hidden: env.VITE_SHOW_VENUE !== 'true'
+    }
+  },
+  {
+    path: '/sponsor',
+    name: 'Sponsor',
+    component: () => import('@/pages/Sponsor.vue'),
+    meta: {
+      order: 5,
+      hidden: env.VITE_SHOW_SPONSOR !== 'true'
+    }
+  },
+  {
+    path: '/community',
+    name: 'Community',
+    component: () => import('@/pages/Community.vue'),
+    meta: {
+      order: 7,
+      hidden: env.VITE_SHOW_COMMUNITY !== 'true'
+    }
+  },
+  {
+    path: '/staff',
+    name: 'Staff',
+    component: () => import('@/pages/Staff.vue'),
+    meta: {
+      order: 8,
+      hidden: env.VITE_SHOW_STAFF !== 'true'
+    }
+  },
+  {
+    name: 'NotFound',
+    path: '/:catchAll(.*)',
+    redirect: '/'
+  }
+]
 
 export const pageRouteNameList = routes.filter(r => !isNaN(Number(r.meta?.order)))
   .sort((a, b) => Number(a.meta?.order) - Number(b.meta?.order))
